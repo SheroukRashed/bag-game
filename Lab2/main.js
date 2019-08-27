ab9161a36ccac3ae124d517370abdc6135a921a7
sheroukRashed <eng_sheroukrashed@yahoo.com>
2019-08-27 18:42:31 +0200

    looping

---
+++ Lab2/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        girls: ["sara", "ola", "carla"],
+        boys: [
+                {name: "omar", age: "21"},
+                {name: "khaled", age: "23"},
+                {name: "kamel", age: "19"},
+            ],
+    },
+});
